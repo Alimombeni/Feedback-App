11fa6ab2221c26358cc80e3190628f346e69e485
alimombeni <ali.mombenni@gmail.com>
2024-04-03 21:42:24 +0330

    feat: [add delete icon and filter in App]

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import FeedbackItem from "./Components/FeedbackItem";
 import FeedbackData from "./Components/data/feedbackData";
 import {useState} from "react";
 import FeedbackList from "./Components/feedbacklist";
+import feedbackItem from "./Components/FeedbackItem";
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
-
+    const deleteFeedback = (id) => {
+     if (window.confirm('Are you sure you want to delete?')) {
+         setFeedback(feedback.filter( (item) => item.id !== id ))
+     }
+    }
 
     return (
       <>
     <Header />
-
         <div className='container'>
-        <FeedbackList feedback={feedback}/>
-
+        <FeedbackList
+            feedback={feedback}
+            handleDelete={deleteFeedback}
+        />
         </div>
-
       </>
   );
 }
