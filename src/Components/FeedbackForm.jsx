eda39285e36511c0a7a60a4940fe000df0e8c4b1
alimombeni <ali.mombenni@gmail.com>
2024-04-08 16:54:37 +0330

    ADD : feedbackform and ratingselect component

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -1,10 +1,12 @@
 import {useState} from "react";
 
 import Card from "./shared/Card";
-
+import RatingSelect from "./RatingSelect";
 function FeedbackForm () {
     //textSTATE for keep input user character
     const [text, setText]=useState('')
+    //ratingSTATE for keep rating 1 to 10
+    const [rating, setRating]=useState(10)
     //btnSTATE for keep button disabled
     const [btnDisabled, setBtnDisabled]=useState(true)
     //messageSTATE for showing text message 10 character checking
@@ -32,7 +34,7 @@ function FeedbackForm () {
         <Card>
             <form>
                 <h2>How would you rate your service with us?</h2>
-            {/* @todo - rating select component */}
+          <RatingSelect select={(rating) =>setRating(rating)} />
             <div className="input-group">
                 <input onChange={handleTextChange} value={text} type="text" placeholder='write a review' />
                 <button
