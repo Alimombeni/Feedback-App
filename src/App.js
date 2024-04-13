b04ea1507a9dabe39f43c1adde98f928f26678f6
alimombeni <ali.mombenni@gmail.com>
2024-04-13 16:14:52 +0330

    ADD:[fetch data from server localhost:5000 ]

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import FeedbackList from "./Components/FeedbackList";
 import FeedbackStats from "./Components/FeedbackStats";
 import FeedbackForm from "./Components/FeedbackForm";
-
 import AboutPage from "./Components/pages/AboutPage";
 import AboutIconLink from "./Components/AboutIconLink";
 import {FeedbackProvider} from "./Context/FeedbackContext";
 
 function App() {
 
-
-
     return (
-
             <FeedbackProvider>
-
         <Router>
             <Header/>
             <div className='container'>
@@ -30,13 +25,11 @@ function App() {
                     <Route path='/about' element={<AboutPage/>}/>
                 </Routes>
             </div>
-<AboutIconLink/>
+            <AboutIconLink/>
         </Router>
-
             </FeedbackProvider>
     );
 }
-
 export default App;
 
 
--- src/Context/FeedbackContext.js
+++ src/Context/FeedbackContext.js
@@ -1,76 +1,63 @@
-import {createContext , useState} from "react";
+import {createContext, useState, useEffect} from "react";
 import {v4 as uuidv4} from "uuid";
 //export default
 const FeedbackContext = createContext()
 
 //this line export for use in App
 export const FeedbackProvider = ({children}) => {
-    const [feedback , setFeedback] = useState([
-        {
-        id:1,
-        text : 'This is feedback item 1',
-        rating:10
-    },
-        {
-            id:2,
-            text : 'This is feedback item 2',
-            rating:8
-        },
-        {
-            id:3,
-            text : 'This is feedback item 3',
-            rating:4
-        },
-    ])
-const [feedbackEdit, setFeedbackEdit] = useState({
-    item: {},
-    edit: false
-})
+    const [isLoading,setIsLoading] = useState(true)
+    const [feedback, setFeedback] = useState([])
+    const [feedbackEdit, setFeedbackEdit] = useState({
+        item: {},
+        edit: false
+    })
+    useEffect(() => {
+        fetchFeedback()
+    }, [])
+//Fetch feedback
+    const fetchFeedback = async () => {
+        const response = await fetch(`http://localhost:5000/feedback?_sort=id`)
+        const data = await response.json()
+        setFeedback(data)
+        setIsLoading(false)
+    }
+
     //update feedback
-
-const updateFeedback = (id , updItem) =>{
-      setFeedback(feedback.map((item) => item.id === id ? {...item , ...updItem} : item))
-}
-
-
+    const updateFeedback = (id, updItem) => {
+        setFeedback(feedback.map((item) => item.id === id ? {...item, ...updItem} : item))
+    }
 //editFeedback function
     const editFeedback = (item) => {
-        setFeedbackEdit( {
+        setFeedbackEdit({
             item,
             edit: true
         })
     }
-
 //deleteFeedback function delete
     const deleteFeedback = (id) => {
         if (window.confirm('Are you sure you want to delete?')) {
-            setFeedback(feedback.filter( (item) => item.id !== id ))
+            setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
 // addFeedback function add
     const addFeedback = (newFeedback) => {
         newFeedback.id = uuidv4()
-        setFeedback([newFeedback , ...feedback ])
+        setFeedback([newFeedback, ...feedback])
     }
-
     //FeedbackProvider
     return <FeedbackContext.Provider
-        value={{feedback,
+        value={{
+            feedback,
             feedbackEdit,
+            isLoading,
             deleteFeedback,
             addFeedback,
             editFeedback,
             updateFeedback,
-    }}>
+        }}>
         {children}
     </FeedbackContext.Provider>
 }
 export default FeedbackContext
-
-
-
-
-
-
 
 
